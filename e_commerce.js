fd49a6d4b93a856f785c5573f84bc53c8f7875f9
Bilal Ahmad <bilalmomand709@gmail.com>
2024-03-15 16:26:26 +0500

    added cross button and some styles

--- e_commerce.js
+++ e_commerce.js
@@ -1,8 +1,14 @@
 const bar= document.getElementById('hamburger_bar');
+const close= document.getElementById('close');
 const nav= document.getElementById('nav_bar');
 
 if(hamburger_bar){
     bar.addEventListener('click', ()=>{
         nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
     })
 }
